c8915d64ba403e6bb3260ad36aea0c629c838e0a
goid <romamedv298@gmail.com>
2024-06-26 21:55:16 +0300

    dsafdsf

--- src/components/WheelComponent/WheelComponent.jsx
+++ src/components/WheelComponent/WheelComponent.jsx
@@ -1,21 +1,83 @@
 import {Wheel} from './spin-wheel-main/dist/spin-wheel-esm.js';
 import { React, useRef, useEffect } from 'react';
-
+import {AlignText} from './spin-wheel-main/src/constants.js';
+import overlayImg from '../../img/svg.svg'
+import tonImg from '../../img/ton_symbol.svg'
 import './WheelComponent.css'
 
 
-const WheelComponent = (props) =>{
+const WheelComponent = () =>{
   const wheelRef = useRef(null);
   const wheelInstanceRef = useRef(null);
  
   useEffect(() => {
     
- 
+    const props = {
+      items: [
+        { image: tonImg,
+          imageRadius: 0.6,
+          imageScale: 2.2,
+        },
+        { image: tonImg,
+          imageRadius: 0.6,
+          imageScale: 2.2,
+        },
+        { image: tonImg,
+          imageRadius: 0.6,
+          imageScale: 2.2,
+        },
+        { image: tonImg,
+          imageRadius: 0.6,
+          imageScale: 2.2,
+        },
+        { image: tonImg,
+          imageRadius: 0.6,
+          imageScale: 2.2,
+        },
+        { image: tonImg,
+          imageRadius: 0.6,
+          imageScale: 2.2,
+        },
+        { image: tonImg,
+          imageRadius: 0.6,
+          imageScale: 2.2,
+        },
+        { image: tonImg,
+          imageRadius: 0.6,
+          imageScale: 2.2,
+        },
+        { image: tonImg,
+          imageRadius: 0.6,
+          imageScale: 2.2,
+        },
+        { image: tonImg,
+          imageRadius: 0.6,
+          imageScale: 2.2,
+        },
+  
+      
+      ],
+      radius: 0.84,
+  
+      itemLabelRadiusMax: 0.35,
+      itemLabelRotation: 180,
+      itemLabelAlign: AlignText.left,
+     
+      itemLabelBaselineOffset: -0.07,
+      itemLabelFont: 'Amatic SC',
+      itemLabelFontSizeMax: 55,
+      itemBackgroundColors: ['#ffc93c', '#66bfbf', '#a2d5f2', '#515070', '#43658b', '#ed6663', '#d54062' ],
+      rotationSpeedMax: 500,
+      rotationResistance: -100,
+      lineWidth: 1,
+      lineColor: '#fff',
+      overlayImage:overlayImg,
+    };
 
    
 
  
-      wheelInstanceRef.current = new Wheel(wheelRef.current, props.props);
+      wheelInstanceRef.current = new Wheel(wheelRef.current, props);
       wheelInstanceRef.current.rotationResistance = 0
   
       wheelInstanceRef.current.spin(10)
