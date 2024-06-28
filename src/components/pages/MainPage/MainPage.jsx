804f59efe581483b01d999df84851f8a23b6388a
romamedv <romamedv298@gmail.com>
2024-06-28 12:21:13 +0400

    adsfdsf

--- src/components/pages/MainPage/MainPage.jsx
+++ src/components/pages/MainPage/MainPage.jsx
@@ -3,12 +3,12 @@ import WheelMain from "../../WheelMain/WheelMain.jsx";
 import './MainPage.css'
 const tg = window.Telegram.WebApp
 const MainPage = () =>{
- 
+    console.log(tg.initDataUnsafe)
   
     return(
         <div className="mainpage">
             <div className="data">
-            {tg?.initDataUnsafe}
+          
             </div>
            
             <WheelMain/>
