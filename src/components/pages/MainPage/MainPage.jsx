cabf5cfccf2abb7a89727f880dc8179cfbfb567f
romamedv <romamedv298@gmail.com>
2024-06-28 11:19:46 +0400

    adsfads

--- src/components/pages/MainPage/MainPage.jsx
+++ src/components/pages/MainPage/MainPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import WheelMain from "../../WheelMain/WheelMain.jsx";
 import './MainPage.css'
+const tg = window.Telegram.WebApp
 const MainPage = () =>{
+    
     return(
         <div className="mainpage">
-          
+            {tg?.version}
             <WheelMain/>
         </div>
     )
