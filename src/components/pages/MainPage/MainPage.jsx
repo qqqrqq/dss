d8b048d1423c43260d47747a912106fefe71e835
goid <romamedv298@gmail.com>
2024-06-29 12:41:51 +0300

    dafds

--- src/components/pages/MainPage/MainPage.jsx
+++ src/components/pages/MainPage/MainPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import WheelMain from "../../WheelMain/WheelMain.jsx";
 import './MainPage.css'
+import defaultProfPic from '../../../img/proficon.png'
 const tg = window.Telegram.WebApp
 const MainPage = () =>{
     console.log(tg.initDataUnsafe)
@@ -8,6 +9,7 @@ const MainPage = () =>{
     return(
         <div className="mainpage">
             <div className="mainpage_info-user">
+        <img src={defaultProfPic} alt="" />
              {tg?.initDataUnsafe?.user?.first_name || 'username'}
             </div>
            
